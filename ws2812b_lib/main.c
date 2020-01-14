d3e85f3aa2b44dd5d04892ac0405b9ae285825b1
Alexander Ivlev <vealvy@yandex.ru>
2020-01-14 21:34:12 +0700

    bugfix

--- ws2812b_lib/main.c
+++ ws2812b_lib/main.c
@@ -13,16 +13,18 @@
 #include <math.h>
 
 #define LED_QUANTITY 150
-#define LED_IN_SEED 20
+#define LED_IN_SEED 60
+
+#define MOOVE_SPEED 10
 
 #define START_R 0xff
 #define START_G 0x00
 #define START_B 0x00
 #define END_R 0x00
 #define END_G 0x00
-#define END_B 0xff
+#define END_B 0x00
 
-#define COMPRESS_VALUE 5.0
+#define COMPRESS_VALUE 7.0
 
 #define ClearOutBit PORTC &= ~(1<<1)
 #define SetOutBit   PORTC |= (1<<1)
@@ -64,15 +66,18 @@ void setLedValues(void)
 	}
 }
 
+float map(float x, float in_min, float in_max, float out_min, float out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 // ¬озвращает рандомное число от -1.0 до 1.0
 float randomVector()
 {
 	int randomIntValue = random()%100;
 	float randomValue  = (float)randomIntValue;
 
-	randomValue = (randomValue - 0.0) * (1.0 - (-1.0)) / (100.0 - 0.0) - 1.0;
-
-	return randomValue;
+	return map(randomValue, 0.0, 100.0, -1.0, 1.0);
 }
 
 float prevVector, nextVector;
@@ -91,8 +96,8 @@ void countNextColorPosition(void)
 {				   
 	float firstPoint  = prevVector*abcisePosition;
 	float secondPoint = nextVector*(abcisePosition - 1.0);
-	interpolatedPoint = (2.0*(firstPoint - secondPoint)*powf(abcisePosition, 4.0) - (3.0*firstPoint - 5.0*secondPoint)*powf(abcisePosition, 3.0) - 3*secondPoint*powf(abcisePosition, 2.0) + firstPoint*abcisePosition)*COMPRESS_VALUE;
-	interpolatedPoint = ((expf(interpolatedPoint*COMPRESS_VALUE)-expf(-1*interpolatedPoint*COMPRESS_VALUE)))/(expf(interpolatedPoint*COMPRESS_VALUE)+expf(-1*interpolatedPoint*COMPRESS_VALUE));
+	interpolatedPoint = (2.0*(firstPoint - secondPoint)*powf(abcisePosition, 4.0) - (3.0*firstPoint - 5.0*secondPoint)*powf(abcisePosition, 3.0) - 3.0*secondPoint*powf(abcisePosition, 2.0) + firstPoint*abcisePosition);
+	interpolatedPoint = (expf(interpolatedPoint*COMPRESS_VALUE)-expf(-1*interpolatedPoint*COMPRESS_VALUE))/(expf(interpolatedPoint*COMPRESS_VALUE)+expf(-1*interpolatedPoint*COMPRESS_VALUE));
 
 	abcisePosition += 1.0/LED_IN_SEED;
 
@@ -103,26 +108,21 @@ void countNextColorPosition(void)
 	}
 }
 
-float map(float x, float in_min, float in_max, float out_min, float out_max)
-{
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 unsigned long int getRed()
 {
 	long int intervalColorR  = (long int)(END_R - START_R);
-	return (unsigned long int)((float)(intervalColorR*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0))  + START_R)*0x100;
+	return (unsigned long int)((float)(intervalColorR*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0)) + START_R)*0x100;
 }
 
 unsigned long int getGreen()
 {
 	long int intervalColorG  = (long int)(END_G - START_G);
-	return (unsigned long int)((float)(intervalColorG*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0))  + START_G)*0x10000;
+	return (unsigned long int)((float)(intervalColorG*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0)) + START_G)*0x10000;
 }
 
 unsigned long int getBlue() {
 	long int intervalColorB  = (long int)(END_B - START_B);
-	return (unsigned long int)((float)(intervalColorB*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0))  + START_B);
+	return (unsigned long int)((float)(intervalColorB*map(interpolatedPoint, -1.0, 1.0, 0.0, 1.0)) + START_B);
 }
 
 // сдвиг вправо с рассчетом нового элемента
@@ -163,7 +163,7 @@ int main(void)
 		countNextColorPosition();
 		moveArray();
 
-		_delay_ms(50);
+		_delay_ms(MOOVE_SPEED);
     }
 }
 
